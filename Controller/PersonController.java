0a51098c54b52115a8527ef76624476fdc9b12b6
Antonio <10717798@polimi.it>
2023-03-09 00:56:42 +0100

    Changes

--- Controller/PersonController.java
+++ Controller/PersonController.java
@@ -1,4 +1,30 @@
 package Controller;
 
+import Model.Person;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class PersonController {
+    private Map<String, Person> persons;
+
+    public PersonController() {
+        this.persons = new HashMap<>();
+    }
+
+    public void addPerson(Person person) {
+        persons.put(person.getName(), person);
+    }
+
+    public Person getPerson(String name) {
+        return persons.get(name);
+    }
+
+    public void removePerson(String name) {
+        persons.remove(name);
+    }
+
+    public boolean personExists(String name) {
+        return persons.containsKey(name);
+    }
 }
