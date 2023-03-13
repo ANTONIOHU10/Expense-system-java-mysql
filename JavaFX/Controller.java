bc95ee6ef6537498b8d499cb28378d153151f92b
Antonio <10717798@polimi.it>
2023-03-13 01:25:17 +0100

    Changes

---
+++ JavaFX/Controller.java
@@ -0,0 +1,23 @@
+package JavaFX;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+public class Controller {
+    @FXML
+    private Button button;
+
+
+    @FXML
+    private Scene scene;
+
+    public void setScene(Scene scene) {
+        this.scene = scene;
+    }
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("Salve!");
+    }
+}
---
+++ Test/Controller/ExpenseControllerTest.java
@@ -0,0 +1,44 @@
+package Test.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpenseControllerTest {
+
+    @Test
+    void insertExpense() {
+    }
+
+    @Test
+    void getUsernames() {
+    }
+
+    @Test
+    void updateBalanceFromExpense() {
+    }
+
+    @Test
+    void setBalance() {
+    }
+
+    @Test
+    void payExpense() {
+    }
+
+    @Test
+    void isExpensePaid() {
+    }
+
+    @Test
+    void consultListExpense() {
+    }
+
+    @Test
+    void consultAllExpenses() {
+    }
+
+    @Test
+    void consultAllBalances() {
+    }
+}
